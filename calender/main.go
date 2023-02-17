884fde9de8746d444ce09ff94c269fbf619bf4a1
hiscat <1251723871@qq.com>
2023-02-17 16:57:54 +0800

    calender

--- calender/main.go
+++ calender/main.go
@@ -1,13 +1,91 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
-func main() {
-	now := time.Now()
-	for i := 0 + 1; i <= DaysOfMonth(now); i++ {
+var (
+	year  int
+	month int
+	day   int
+	now   = time.Now()
+)
 
+func init() {
+	flag.IntVar(&year, "year", now.Year(), "year of calender. default current year")
+	flag.IntVar(&month, "month", int(now.Month()), "month of calender. default current month")
+	flag.IntVar(&day, "day", now.Day(), "day of calender. default current day")
+}
+
+func main() {
+	flag.Parse()
+
+	now = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+
+	fmt.Println(now.YearDay(), now.Day(), ChineseOfWeekday(int(now.Weekday())))
+	fmt.Println(getCalenderOfMonth(now))
+
+	for i := 1; i <= 12; i++ {
+		date := time.Date(now.Year(), time.Month(i), 10, 0, 0, 0, 0, time.Local)
+		fmt.Println(date, ChineseOfWeekday(int(date.Weekday())))
+		//fmt.Println(getCalenderOfMonth(date))
+	}
+
+}
+
+func getCalenderOfMonth(now time.Time) *strings.Builder {
+	firstDay := firstDayOfMonth(now)
+	s := &strings.Builder{}
+	for i := 1; i <= 7; i++ {
+		s.WriteString(fmt.Sprintf("%s\t", ChineseOfWeekday(i)))
+	}
+	s.WriteString("\n")
+	s.WriteString(tabsOfWeekday(firstDay.Weekday()))
+	for i, j := 0+1, firstDay.Weekday(); i <= DaysOfMonth(now); i++ {
+		s.WriteString(fmt.Sprintf("%d\t", i))
+		if j%7 == 0 {
+			s.WriteString("\n")
+		}
+		j++
+	}
+	return s
+}
+
+func tabsOfWeekday(weekday time.Weekday) string {
+	switch weekday {
+	case time.Monday, time.Tuesday, time.Thursday, time.Wednesday, time.Friday, time.Saturday:
+		return strings.Repeat("\t", int(weekday)-1)
+	default:
+		return strings.Repeat("\t", 6)
+	}
+}
+
+func firstDayOfMonth(now time.Time) time.Time {
+	return now.AddDate(0, 0, -now.Day()+1)
+}
+
+func ChineseOfWeekday(i int) string {
+	switch i {
+	case 1:
+		return "周一"
+	case 2:
+		return "周二"
+	case 3:
+		return "周三"
+	case 4:
+		return "周四"
+	case 5:
+		return "周五"
+	case 6:
+		return "周六"
+	case 7, 0:
+		return "周日"
+	default:
+		panic("unknown weekday " + strconv.Itoa(i))
 	}
 }
 
