34ad5486425ab4ce5604b38f68008920706aeee1
hiscat <1251723871@qq.com>
2023-02-21 16:27:50 +0800

    pro-go/27. Using Reflection

--- pro-go/27. Using Reflection /reflection/17_test.go
+++ pro-go/27. Using Reflection /reflection/17_test.go
@@ -1,27 +1,28 @@
 package main
 
-import (
-	"math/rand"
-	"testing"
-	"time"
-)
-
-var ints = make([]int, 1000)
-
-func TestMain(m *testing.M) {
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 1000; i++ {
-		ints[i] = i
-	}
-	m.Run()
-}
-func Benchmark_contains(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		contains(ints, rand.Intn(1000))
-	}
-}
-func Benchmark_contains2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		contains2(ints, rand.Intn(1000))
-	}
-}
+//
+//import (
+//	"math/rand"
+//	"testing"
+//	"time"
+//)
+//
+//var ints = make([]int, 1000)
+//
+//func TestMain(m *testing.M) {
+//	rand.Seed(time.Now().UnixNano())
+//	for i := 0; i < 1000; i++ {
+//		ints[i] = i
+//	}
+//	m.Run()
+//}
+//func Benchmark_contains(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		contains(ints, rand.Intn(1000))
+//	}
+//}
+//func Benchmark_contains2(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		contains2(ints, rand.Intn(1000))
+//	}
+//}
